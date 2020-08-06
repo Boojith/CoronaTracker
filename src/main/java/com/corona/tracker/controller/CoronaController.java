d1e5273ec0cf983f8c6322de9d2b3d4bfe20b1e4
Boojith K <boojith22@gmail.com>
2020-08-06 22:09:05 +0530

    Initial commit

---
+++ src/main/java/com/corona/tracker/controller/CoronaController.java
@@ -0,0 +1,44 @@
+package com.corona.tracker.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+import com.corona.tracker.model.Corona;
+
+@Controller
+public class CoronaController {
+	@RequestMapping(value = "/corona")
+	public String getStatus(Model model) {
+		final String uri = "https://corona-virus-stats.herokuapp.com/api/v1/cases/countries-search?limit=200";
+
+		RestTemplate restTemplate = new RestTemplate();
+		String result = restTemplate.getForObject(uri, String.class);
+		JSONObject object = new JSONObject(result);
+		String date=object.getJSONObject("data").getString("last_update");
+		JSONArray rows = object.getJSONObject("data").getJSONArray("rows");
+		List<Corona> coronaList = new ArrayList<Corona>();
+		for (int i = 0; i < rows.length(); i++) {
+			JSONObject row = rows.getJSONObject(i);
+			Corona corona = new Corona();
+			corona.setCountry(row.getString("country"));
+			corona.setTotalCases(row.getString("total_cases"));
+			corona.setNewCases(row.getString("new_cases"));
+			corona.setTotalDeaths(row.getString("total_deaths"));
+			corona.setNewDeaths(row.getString("new_deaths"));
+			corona.setTotalRecovered(row.getString("total_recovered"));
+			corona.setActiveCases(row.getString("active_cases"));
+			coronaList.add(corona);
+
+		}
+		model.addAttribute("coronaList", coronaList);
+		model.addAttribute("date", date);
+		return "status/corona.html";
+	}
+}
---
+++ src/main/resources/static/js/custom.js
@@ -0,0 +1,6 @@
+/**
+ * Custom JS
+ */
+$(document).ready(function() {
+    $('#coronaTable').DataTable();
+} );
